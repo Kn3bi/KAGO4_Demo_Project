13ed680bbafad0d941d88f57d4c363dbae74b4f2
KNB <knb@kkg-do.eu>
2021-11-18 09:37:00 +0100

    Erste Version Einfaches Spiel zur Motivation! Zielen mit Links/Rechts, Losfliegen mit Leertaste

--- src/main/java/my_project/model/Spaceship.java
+++ src/main/java/my_project/model/Spaceship.java
@@ -1,5 +1,7 @@
 package my_project.model;
 
+import KAGO_framework.control.SoundController;
+import KAGO_framework.control.ViewController;
 import KAGO_framework.model.InteractiveGraphicalObject;
 import KAGO_framework.view.DrawTool;
 
@@ -7,22 +9,47 @@ import java.awt.event.KeyEvent;
 
 public class Spaceship extends InteractiveGraphicalObject {
 
-    public Spaceship(double x, double y) {
-        this.x = x;
-        this.y = y;
+    private double rotation;
+    private boolean start;
+
+    public Spaceship() {
         this.setNewImage("src/main/resources/graphic/spaceship.png");
-        //this.getMyImage()
+        reset();
+    }
+
+    public void reset(){
+        x = 50;
+        y = 500;
+        rotation = Math.random()*100-30;
+        start = false;
     }
 
     @Override
     public void draw(DrawTool drawTool){
-        drawTool.drawImage(getMyImage(),x,y);
+        drawTool.drawTransformedImage(getMyImage(),x,y,rotation,1);
+    }
+
+    @Override
+    public void update(double dt){
+        if (ViewController.isKeyDown(KeyEvent.VK_LEFT) && !start){
+            rotation = rotation - 50*dt;
+        }
+        if (ViewController.isKeyDown(KeyEvent.VK_RIGHT) && !start){
+            rotation = rotation + 50*dt;
+        }
+        if(start){
+            moveInDirection(rotation,250,dt);
+        }
+        if(start && (x > 800 || x < -200 || y > 800 || y < -200)){
+            reset();
+        }
     }
 
     @Override
     public void keyReleased(int key){
-        if(key == KeyEvent.VK_SPACE){
-
+        if(key == KeyEvent.VK_SPACE && !start){
+            start  = true;
+            SoundController.playSound("whoosh");
         }
     }
 
